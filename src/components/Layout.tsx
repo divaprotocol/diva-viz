0235599f143165670de567f9b34eb5a690245162
Tarun yadav <56935638+tarunyadav1@users.noreply.github.com>
2023-03-23 13:31:34 +0530

    add: handle the expire offer and add anmation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,13 +3,13 @@ import Header from './Header'
 
 const Layout = ({ children }) => {
 	return (
-		<div className="bg-black h-screen relative">
+		<div className="bg-black h-screen relative w-screen">
 			<div className="absolute top-0 left-0 pointer-events-none">
 				<img src={'./bg-pattern.svg'} alt="pattern-wave" />
 			</div>
 			<Header />
 			{children}
-			<div className="absolute -right-56 -bottom-1/2 pointer-events-none">
+			<div className="fixed -right-56 -bottom-1/2 pointer-events-none">
 				<img src={'./globe.svg'} alt="globe-vector" />
 			</div>
 		</div>
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
+import { staggerContainer, textVariant } from '../utils/motion'
+import { motion } from 'framer-motion'
 
 const home = () => {
 	const [isButtonClicking, setIsButtonClicking] = React.useState<boolean>(false)
@@ -30,43 +32,57 @@ const home = () => {
 	}
 
 	return (
-		<div className="text-white flex flex-col justify-center items-center mt-20">
-			<div className="flex flex-col justify-center items-center font-cirka leading-relaxed tracking-widest">
-				<div className="text-6xl font-bold mb-3">Share your offers</div>
-				<div className="text-6xl font-bold flex">
-					with the
-					<div className="flex ml-3 relative">
-						<div>world</div>
-						<div className="top-[1.55rem] right-[4.4rem] absolute pointer-events-none">
-							<img src="./o-vector.svg" alt="o" width={34} />
+		<motion.div
+			variants={staggerContainer(0.1, 1)}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: true, amount: 0.1 }}>
+			<div className="text-white flex flex-col justify-center items-center mt-20">
+				<div className="flex flex-col justify-center items-center font-cirka leading-relaxed tracking-widest">
+					<motion.div variants={textVariant(1)}>
+						<div className="text-6xl font-bold mb-3">Share your offers</div>
+					</motion.div>
+					<motion.div variants={textVariant(1)}>
+						<div className="text-6xl font-bold flex">
+							with the
+							<div className="flex ml-3 relative">
+								<div>world</div>
+								<div className="top-[1.55rem] right-[4.4rem] absolute pointer-events-none">
+									<img src="./o-vector.svg" alt="o" width={34} />
+								</div>
+								<div className="absolute right-[-1.15rem] top-[-0.5rem]">
+									<img src="./star-vector.svg" alt="star" />
+								</div>
+							</div>
 						</div>
-						<div className="absolute right-[-1.15rem] top-[-0.5rem]">
-							<img src="./star-vector.svg" alt="star" />
-						</div>
-					</div>
+					</motion.div>
 				</div>
-			</div>
 
-			<div className="mt-6 text-xl font-text font-light text-[#D2D2D2] relative">
-				<div className="absolute bottom-[-0.6rem] left-[-1rem]">
-					<img src="./star-vector.svg" alt="star" width={25} />
-				</div>
-				Generate your offer post to share with the world
-			</div>
-			<div className="mt-11">
-				<input
-					className={`w-[28.75rem] h-[3.75rem] border-[0.4px] ${
-						error ? 'border-red-400' : 'border-x-sky-50'
-					}  bg-black placeholder:text-center uppercase text-center focus:placeholder-transparent font-text font-bold focus:outline-none text-xs tracking-widest placeholder:tracking-widest text-[#8A8A8A]`}
-					type="text"
-					placeholder="ENTER YOUR OFFER HASH HERE"
-					value={inputPoolId}
-					onChange={(e) => setInputPoolId(e.target.value)}
-					ref={inputElRef}
-				/>
-			</div>
-			{/* commented for future use */}
-			{/* <div className="mt-2">
+				<motion.div variants={textVariant(1)}>
+					<div className="mt-6 text-xl font-text font-light text-[#D2D2D2] relative">
+						<div className="absolute bottom-[-0.6rem] left-[-1rem]">
+							<img src="./star-vector.svg" alt="star" width={25} />
+						</div>
+						Generate your offer post to share with the world
+					</div>
+				</motion.div>
+
+				<motion.div variants={textVariant(1)}>
+					<div className="mt-11">
+						<input
+							className={`w-[28.75rem] h-[3.75rem] border-[0.4px] ${
+								error ? 'border-red-400' : 'border-x-sky-50'
+							}  bg-black placeholder:text-center uppercase text-center focus:placeholder-transparent font-text font-bold focus:outline-none text-xs tracking-widest placeholder:tracking-widest text-[#8A8A8A]`}
+							type="text"
+							placeholder="ENTER YOUR OFFER HASH HERE"
+							value={inputPoolId}
+							onChange={(e) => setInputPoolId(e.target.value)}
+							ref={inputElRef}
+						/>
+					</div>
+				</motion.div>
+				{/* commented for future use */}
+				{/* <div className="mt-2">
 				<div className="border-2 border-[#8A8A8A] flex gap-2 p-1 uppercase font-text text-xs">
 					<div
 						className={`${
@@ -88,24 +104,28 @@ const home = () => {
 					</div>
 				</div>
 			</div> */}
-			<div className="mt-2 h-3">
-				{error && (
-					<div className="text-xs text-red-400 font-text font-bold">
-						{error}
+
+				<div className="mt-2 h-3">
+					{error && (
+						<div className="text-xs text-red-400 font-text font-bold">
+							{error}
+						</div>
+					)}
+				</div>
+				<motion.div variants={textVariant(1)}>
+					<div
+						className="mt-9 overflow-hidden cursor-pointer "
+						onMouseDown={() => setIsButtonClicking(true)}
+						onMouseUp={() => setIsButtonClicking(false)}
+						style={{
+							transform: isButtonClicking ? 'scale(0.95)' : 'scale(1)',
+						}}
+						onClick={handleClick}>
+						<img src="./post-btn.svg" />
 					</div>
-				)}
+				</motion.div>
 			</div>
-			<div
-				className="mt-9 overflow-hidden cursor-pointer "
-				onMouseDown={() => setIsButtonClicking(true)}
-				onMouseUp={() => setIsButtonClicking(false)}
-				style={{
-					transform: isButtonClicking ? 'scale(0.95)' : 'scale(1)',
-				}}
-				onClick={handleClick}>
-				<img src="./post-btn.svg" />
-			</div>
-		</div>
+		</motion.div>
 	)
 }
 
